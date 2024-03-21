43da029df2626483753f9b2b228a6209b70cd0ab
awfulbananas <Joseph.Fromel@gmail.com>
2024-03-21 11:07:53 -0700

    added javadoc comments in Frindow

--- src/fromics/Frindow.java
+++ src/fromics/Frindow.java
@@ -10,12 +10,17 @@ import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//a class representing a window for drawing on.
-//doesn't actually need the rest of this library to be useful, besides Keys and Mouse
-//maybe these three should go in a different package
+/**
+ * Frindow is a class representing a window on the screen which covers much
+ * of the boilerplate otherwise necessary in a jav program, as well as facilitating
+ * more convenient frame drawing and input management.
+ * you shouldn't really interact with this class much when making a program besides
+ * feeding it to constructors that want it and initializing one to begin with
+ * @author Joseph Fromel
+ */
 @SuppressWarnings("serial")
 public class Frindow extends Panel {
-public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+	public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
 	//the Frame which goes on the screen
 	private Frame frame;
 	//the frame buffer, so that there isn't screen flickering with asynchronous operation
@@ -36,19 +41,29 @@ public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphics
 	private Graphics initG;
 	//whether or not a frame is currently being painted to the screen
 	private boolean painting;
-	
-	//a class for managing frame closure, so that the window actually closes when you hit the X button
+
+	/**
+	 * This class exists solely to make sure the window closes when you hit
+	 * the close button or alt-f4, since java windows don't do that by default
+	 */
 	private class WindowOperator extends WindowAdapter {
 		//the Frame for the WindowOperator
 		Frame f;
-		
-		//constructs a new WindowOperator for the given Frame
+
+		/**
+		 * constructs a new WindowOperator for the given frame
+		 * @param parent the frame this WindowOperator will be operating on
+		 */
 		public WindowOperator(Frame parent) {
 			f = parent;
 			f.addWindowListener(this);
 		}
-		
-		//called when the x button of the window is clicked
+
+		/**
+		 * closes the window, executes closing behavior,
+		 * and exits the program when a window closing event is processes
+		 * @param e the event to be processed
+		 */
 		@Override
 		public void windowClosing (WindowEvent e) { 
 			f.dispose();
@@ -56,20 +71,32 @@ public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphics
             System.exit(0);
         }  
 	}
-	
-	
-	//returns the current BufferedImage color model of this Frindow
+
+
+	/**
+	 * returns the BufferedImage color type being used to draw frames
+	 * @return the current color type of this Frindow
+	 */
 	public int getColorType() {
 		return colorType;
 	}
-	
-	//called regularly to keep up with keystroke events
+
+	/**
+	 * called regularly to process input events
+	 */
 	public void update() {
 		keys.process();
 		mouse.processAll();
 	}
-	
-	//constructs a new Frindow in the given color space of size (width, height)
+
+	/**
+	 * constructs a new Frindow with the given colorType for drawing frames,
+	 * initial width and height of the window, and window name
+	 * @param colorType the color space to be used when drawing frames
+	 * @param width the initial width of the window (can be resized)
+	 * @param height the initial height of the window (can be resized)
+	 * @param name the name of the window
+	 */
 	public Frindow(int colorType, int width, int height, String name) {
 		contentBuffer = new LinkedList<>();
 		painting = false;
@@ -89,19 +116,37 @@ public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphics
 		frame.setFocusable(true);
 		setFocusable(true);
 	}
-	//constructs a new Frindow in the given color space of size (width, height)
+
+	/**
+	 * constructs a new Frindow with the given color type for drawing frames,
+	 * initial width and height of the window, and a default window name of "game"
+	 * @param colorType the color space to be used when drawing frames
+	 * @param width the initial width of the window (can be resized)
+	 * @param height the initial height of the window (can be resized)
+	 */
 	public Frindow(int colorType, int width, int height) {
 		this(colorType, width, height, "game");
 	}
-	
-	//constructs a new Frindow in TYPE_INT_RGB color space, with width and height based on screen size
+
+	/**
+	 * constructs a new Frindow with integer RGB color space for drawing frames,
+	 * an initial width and height proportional to the device screen size,
+	 * and a window name of "game"
+	 */
 	public Frindow() {
 		this(BufferedImage.TYPE_INT_RGB, SCREEN_RECT.width, SCREEN_RECT.height);
 	}
-	
-	//this should be called before this Frindow is drawn on, 
-	//this makes the Frindow visible, and sets the number of buffer frames to bufferCount
-	//returns the graphics which should be used when calling .paint()
+
+	/**
+	 * initializes some functionality of the Frindow, including making the
+	 * window visible, returning a useless Graphics object I forgot to get rid of(TODO:fix that)
+	 * @param bufferCount the size of the frame buffer when drawing frames. a larger buffer creates
+	 *                    more delay between updates and changes on screen while a smaller buffer can create
+	 *                    screen tearing, so  find that 3 is usually a good midpoint
+	 * @param game the Manager object which is used when closing the window to execute closing functionality
+	 *             and when drawing frames to the screen
+	 * @return a Graphics object which is mostly useless which I forgot to get rid of returning
+	 */
 	public Graphics init(int bufferCount, Manager game) {
 		this.game = game;
 		for(int i = 0; i < bufferCount; i++) contentBuffer.add(new BufferedImage(getWidth(), getHeight(), colorType));
@@ -111,17 +156,23 @@ public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphics
 		initG = getGraphics();
 		return initG;
 	}
-	
-	//paints the Frindow with the default Graphics
+
+	/**
+	 * starts a thread which draws the next frame in the frame buffer to the screen
+	 * and uses the drawAll method of the Manager game to create the next frame or the buffer
+	 */
 	public void defPaint() {
 		if(!painting) {
 			painting = true;
 			(new Thread(() -> paint(initG))).start();
 		}
 	}
-	
-	//draws the next frame to the screen,
-	//g should be the graphics returned by .init()
+
+	/**
+	 * creates a new frame, adds it to the frame buffer, then uses the drawAll method
+	 * from the Manger game to draw on that frame
+	 * @param g doesn't actually matter, and I'm realizing I should have cleaned up this class a while ago
+	 */
 	@Override
 	public void paint(Graphics g) {
 		game.drawAll(getNewFrame());
@@ -131,43 +182,68 @@ public static final Rectangle SCREEN_RECT = GraphicsEnvironment.getLocalGraphics
 		g.drawImage(contentBuffer.remove(), 0, 0, this);
 		painting = false;
 	}
-	
-	//adds a new frame to the frame buffer, and returns a Graphics object for drawing on that frame
+
+	/**
+	 * creates a new frame and adds it to the frame buffer, returning the graphics object for
+	 * that frame
+	 * @return the graphics object for the new frame
+	 */
 	private Graphics getNewFrame() {
 		BufferedImage next = new BufferedImage(getWidth(), getHeight(), colorType);
 		contentBuffer.add(next);
 		return next.getGraphics();
 	}
-	
-	//adds a new frame to the frame buffer, and returns that frame as a BufferedImage
+
+	/**
+	 * creates a new BufferedImage and adds it to the frame buffer, returning that BufferedImage
+	 * @return the new BufferedImage to be added to the frame buffer
+	 */
 	public BufferedImage getNewFrameImg() {
 		BufferedImage next = new BufferedImage(getWidth(), getHeight(), colorType);
 		contentBuffer.add(next);
 		return next;
 	}
-	
-	//returns the Keys object for this Frindow
+
+	/**
+	 * returns the Keys object for this Frindow, which helps manage input from the keyboard
+	 * @return the Keys object of his Frindow
+	 * @see Keys
+	 */
 	public Keys getKeys() {
 		return keys;
 	}
-	
-	//returns the the location of a point in the window from a location in global space
+
+	/**
+	 * calculates the position from the upper right corner of the window given a point
+	 * in global screen space
+	 * @param globalLoc a Point in global screen space
+	 * @return a Point representing the location of the given Point relative to the window
+	 */
 	public Point locOnScreenFromGlobalLoc(Point globalLoc) {
 		return globalLoc.copy().sub(this.getLocationOnScreen().x, this.getLocationOnScreen().y);
 	}
-	
-	//returns a Point representing the location of the mouse on the frame
+
+	/**
+	 * gets the position of the mouse on the window
+	 * @return the position of the mouse pointer relative to the upper left corner of the window
+	 */
 	public Point getMousePos() {
 		return locOnScreenFromGlobalLoc(mouse.getMouseLoc());
 	}
-	
-	//adds a function to be run whenever a keystroke is detected,
-	//in this case, the keystroke is for any key rather than the usual
-	//only alphanumeric and symbolic keys
+
+	/**
+	 * registers a KeypressFunction to be run whenever a key is pressed and released,
+	 * giving the function the corresponding KeyEvent as it's argument
+	 * @param func the KeypressFunction which will be run when a key is typed
+	 */
 	public void addKeystrokeFunction(KeypressFunction func) {
 		keys.addKeypressFunction(func);
 	}
 
+	/**
+	 * returns the Mouse object associated with this Frindow, which helps manage input from the mouse
+	 * @return the Mouse object associated with this Frindow
+	 */
 	public Mouse getMouse() {
 		return mouse;
 	}
